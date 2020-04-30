2e16ed81e8feb7aaf5f5c4184871f8200f88406f
Randheer <randheer094@gmail.com>
2020-04-30 20:00:12 +0800

    Leetcode Binary Tree Path Validation Problem

--- src/problems/BinaryTreeMaxPathSum.kt
+++ src/problems/BinaryTreeMaxPathSum.kt
@@ -4,15 +4,12 @@ package problems
  * Created by randheercode
  * Date: 29/4/20
  * Time: 8:25 pm
+ * Statement: Binary Tree Max Path Sum Problem from LeetCode.
  */
 
 class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
-}
-
-class PathValue {
-    var value = 0
 }
 
 class BinaryTreeMaxPathSum {
