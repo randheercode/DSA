3197bcff8062bfa5ad6530090717b31991c5f6ac
Randheer <randheer094@gmail.com>
2020-05-22 18:29:36 +0800

    Leetcode problem

---
+++ src/problems/SortCharByFreq.kt
@@ -0,0 +1,23 @@
+package problems
+
+/**
+ * Created by randheercode
+ * Date: 22/5/20
+ * Time: 6:06 pm
+ * Problem Statement: Given a string, sort it in decreasing order based on the frequency of characters.
+ */
+class SortCharByFreq {
+    fun frequencySort(s: String): String {
+        return s.groupingBy { it }.eachCount().toList().sortedBy { it.second }.reversed().map { c ->
+            c.first.toString().repeat(c.second)
+        }.joinToString("")
+    }
+}
+
+
+fun main() {
+    println(SortCharByFreq().frequencySort("tree"))
+    println(SortCharByFreq().frequencySort("cccaaa"))
+    println(SortCharByFreq().frequencySort("Aabb"))
+}
+
