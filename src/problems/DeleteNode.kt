b5cf52f23325bf09dbd049b993c2bb7bae1e7517
Randheer <randheer094@gmail.com>
2020-06-02 16:17:06 +0800

    Delete Node

---
+++ src/problems/DeleteNode.kt
@@ -0,0 +1,13 @@
+package problems
+
+/**
+ * Created by randheercode
+ * Date: 2/6/20
+ * Time: 4:06 pm
+ */
+class DeleteNode {
+    fun deleteNode(node: ListNode?) {
+        node?.`val` = node?.next?.`val` ?: 0
+        node?.next = node?.next?.next
+    }
+}
