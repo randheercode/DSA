a549ccf4fc66e857173bb2b21bd67a625f3a1444
Randheer <randheer094@gmail.com>
2020-06-03 19:44:43 +0800

    Multiple problem solved

--- src/problems/MultipleProblemLCEasy.kt
+++ src/problems/MultipleProblemLCEasy.kt
@@ -6,6 +6,8 @@ package problems
  * Time: 5:46 pm
  * Problems:
  * https://leetcode.com/problems/reverse-string-ii/
+ * https://leetcode.com/problems/strobogrammatic-number/
+ * https://leetcode.com/problems/string-compression/
  */
 class MultipleProblemLCEasy {
     fun reverseStr(s: String, k: Int): String {
@@ -49,11 +51,63 @@ class MultipleProblemLCEasy {
         }
         return true
     }
+
+    fun compress(chars: CharArray): Int {
+        val length = chars.size
+        var scanIndex = 0
+        var startIndex = 0
+        var writeIndex = 0
+        var currentChar: Char
+        while (scanIndex < length) {
+            currentChar = chars[scanIndex]
+            while (scanIndex + 1 < length && chars[scanIndex + 1] == currentChar) scanIndex += 1
+            val charCount = scanIndex - startIndex + 1
+            if (charCount == 1) {
+                chars[writeIndex] = currentChar
+                writeIndex += 1
+                scanIndex += 1
+                startIndex = scanIndex
+            } else {
+                chars[writeIndex] = currentChar
+                writeIndex += 1
+                val charCountS = charCount.toString()
+                for (i in charCountS.indices) {
+                    chars[writeIndex] = charCountS[i]
+                    writeIndex += 1
+                }
+                scanIndex += 1
+                startIndex = scanIndex
+            }
+        }
+        return writeIndex
+    }
+
+    fun countAndSay(n: Int): String {
+        if (n == 1) return "1"
+        val last = countAndSay(n - 1)
+        val length = last.length
+        val builder = StringBuilder()
+        var start = 0
+        var current = 0
+        var char: Char
+        while (current < length) {
+            char = last[current]
+            while (current + 1 < length && last[current + 1] == char) current += 1
+            val count = current - start + 1
+            builder.append(count).append(char)
+            start = current + 1
+            current = start
+        }
+        return builder.toString()
+
+    }
 }
 
 fun main() {
-    // testReverseString2()
-    testisStrobogrammatic()
+//     testReverseString2()
+//    testisStrobogrammatic()
+//    testStringCompression()
+    testCountAndSay()
 }
 
 private fun testReverseString2() {
@@ -66,5 +120,24 @@ private fun testisStrobogrammatic() {
     println(MultipleProblemLCEasy().isStrobogrammatic("69"))
     println(MultipleProblemLCEasy().isStrobogrammatic("00"))
     println(MultipleProblemLCEasy().isStrobogrammatic("123"))
+}
+
+private fun testStringCompression() {
+    println(MultipleProblemLCEasy().compress(charArrayOf('a', 'a', 'b', 'b', 'c', 'c', 'c')))
+    println(MultipleProblemLCEasy().compress(charArrayOf('a')))
+    println(MultipleProblemLCEasy().compress(charArrayOf('a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b')))
+}
+
+private fun testCountAndSay() {
+    println(MultipleProblemLCEasy().countAndSay(1))
+    println(MultipleProblemLCEasy().countAndSay(2))
+    println(MultipleProblemLCEasy().countAndSay(3))
+    println(MultipleProblemLCEasy().countAndSay(4))
+    println(MultipleProblemLCEasy().countAndSay(5))
+    println(MultipleProblemLCEasy().countAndSay(6))
+    println(MultipleProblemLCEasy().countAndSay(7))
+    println(MultipleProblemLCEasy().countAndSay(8))
+    println(MultipleProblemLCEasy().countAndSay(9))
+    println(MultipleProblemLCEasy().countAndSay(10))
 }
 
