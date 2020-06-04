d45b1ff3dfdebbded7decb9a68e452df80c0504f
Randheer <randheer094@gmail.com>
2020-06-04 19:21:48 +0800

    Multiple problem solved

--- src/problems/MultipleProblemLCEasy.kt
+++ src/problems/MultipleProblemLCEasy.kt
@@ -345,6 +345,17 @@ class MultipleProblemLCEasy {
         return ans
     }
 
+    fun minPathSum(grid: Array<IntArray>): Int {
+        for (i in grid.size - 1 downTo 0) {
+            for (j in grid[0].size - 1 downTo 0) {
+                if (i == grid.size - 1 && j != grid[0].size - 1) grid[i][j] = grid[i][j] + grid[i][j + 1]
+                else if (j == grid[0].size - 1 && i != grid.size - 1) grid[i][j] = grid[i][j] + grid[i + 1][j]
+                else if (j != grid[0].size - 1 && i != grid.size - 1) grid[i][j] = grid[i][j] + minOf(grid[i + 1][j], grid[i][j + 1])
+            }
+        }
+        return grid[0][0]
+    }
+
 }
 
 fun main() {
