572b65f9bc2d6abd7a21e5470eb3797af7df1470
Randheer <randheer094@gmail.com>
2020-06-15 17:53:41 +0800

    Todays challenge

--- src/problems/MultipleProblemLCEasy.kt
+++ src/problems/MultipleProblemLCEasy.kt
@@ -466,6 +466,13 @@ class MultipleProblemLCEasy {
             index = index.plus(countI)
         }
     }
+
+    fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
+        if (root == null) return null
+        if (root.`val` == `val`) return root
+        return if (root.`val` > `val`) searchBST(root.left, `val`)
+        else searchBST(root.right, `val`)
+    }
 }
 
 fun main() {
