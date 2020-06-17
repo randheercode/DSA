1709cebb43bb3c2b9661ca3761c20111a7d42e13
Randheer <randheer094@gmail.com>
2020-06-17 17:41:26 +0800

    Todays challenge

--- src/Utils.kt
+++ src/Utils.kt
@@ -4,12 +4,27 @@
  * Time: 4:42 pm
  */
 fun printArray(image: Array<IntArray>, prefix: String = "") {
-    println(prefix + image.joinToString(separator = ",", prefix = "[", postfix = "]") { it.joinToString(separator = ",", prefix = "[", postfix = "]") })
+    println(prefix)
+    println(image.joinToString(separator = "\n") { it.joinToString(separator = ",\t") })
+    println()
+}
+
+fun printCharArray(image: Array<CharArray>, prefix: String = "") {
+    println(prefix)
+    println(image.joinToString(separator = "\n") { it.joinToString(separator = ",\t") })
+    println()
 }
 
 fun generateIntArray(data: String): Array<IntArray> {
     return data.split("],[").map {
         it.replace("[", "").replace("]", "")
                 .split(",").map { it.toInt() }.toIntArray()
+    }.toTypedArray()
+}
+
+fun generateCharArray(data: String): Array<CharArray> {
+    return data.split("],[").map {
+        it.replace("[", "").replace("]", "")
+                .split(",").map { it[0] }.toCharArray()
     }.toTypedArray()
 }
