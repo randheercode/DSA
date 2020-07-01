cdb60b4953d2318edfe99d7f51a3536aedf66f2a
Randheer <randheer094@gmail.com>
2020-07-01 19:07:57 +0800

    Todays challenge

--- src/problems/MultipleProblemLCEasy.kt
+++ src/problems/MultipleProblemLCEasy.kt
@@ -975,6 +975,27 @@ class MultipleProblemLCEasy {
         return d[m - 1][n - 1]
     }
 
+    fun arrangeCoins(n: Int): Int {
+        return (Math.sqrt(2 * n.toLong() + 0.25) - 0.5).toInt()
+    }
+
+    fun arrangeCoins1(n: Int): Int {
+        var left: Long = 0
+        var right = n.toLong()
+        var k: Long
+        var curr: Long
+        while (left <= right) {
+            k = left + (right - left) / 2
+            curr = k * (k + 1) / 2
+            if (curr == n.toLong()) return k.toInt()
+            if (n < curr) {
+                right = k - 1
+            } else {
+                left = k + 1
+            }
+        }
+        return right.toInt()
+    }
 }
 
 fun main() {
