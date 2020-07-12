693c8432f8b957cf6eefbf73430aa87b2e9e7d15
Randheer <randheer094@gmail.com>
2020-07-12 15:35:11 +0800

    Todays challenge

--- src/problems/MultipleProblemLCEasy.kt
+++ src/problems/MultipleProblemLCEasy.kt
@@ -1228,6 +1228,17 @@ class MultipleProblemLCEasy {
         return maxLen
     }
 
+    fun reverseBits(n: Int): Int {
+        var n = n
+        var ans = 0
+        for (i in 0..31) {
+            ans = ans shl 1
+            ans = ans or (n and 1)
+            n = n shr 1
+        }
+        return ans
+    }
+
 }
 
 fun main() {
