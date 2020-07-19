daf2db57f83517c7fd32c5daf56feafe59bc0c48
Randheer <randheer094@gmail.com>
2020-07-19 17:05:41 +0800

    W198 second q

--- src/problems/lc_contest/w/w198/W198.kt
+++ src/problems/lc_contest/w/w198/W198.kt
@@ -30,38 +30,61 @@ private fun test1() {
 }
 
 private class Prob2 {
-    // Wrong Answer
+    lateinit var res: IntArray
+    val edgesMap = HashMap<Int, MutableList<Int>>()
+    lateinit var labelMap: CharArray
+    lateinit var seen: HashSet<Int>
+
     fun countSubTrees(n: Int, edges: Array<IntArray>, labels: String): IntArray {
-        val count = mutableMapOf<Int, MutableMap<Char, Int>>()
-        repeat(n) {
-            val map = mutableMapOf<Char, Int>()
-            map[labels[it]] = 1
-            count[it] = map
-        }
-        val adj = mutableMapOf<Int, MutableList<Int>>()
+        res = IntArray(n)
+        seen = HashSet(n)
+        labelMap = CharArray(n)
+
+        // create edge map
         edges.forEach {
-            adj[it[0]] = adj.getOrDefault(it[0], mutableListOf())
-            adj[it[0]]?.add(it[1])
+            edgesMap.putIfAbsent(it[0], mutableListOf())
+            edgesMap.putIfAbsent(it[1], mutableListOf())
+
+            // Add edges in both direction. But we use a HashSet to make sure we only
+            // traverse from root to children
+            edgesMap[it[0]]?.add(it[1])
+            edgesMap[it[1]]?.add(it[0])
+
         }
-        for (i in n - 1 downTo 0) {
-            val currentAdj = adj[i] ?: mutableListOf()
-            val label = labels[i]
-            val currentCount = count[i]!!
-            currentAdj.forEach { ad ->
-                val adjCount = count[ad]!!
-                adjCount.forEach {
-                    val adjKey = it.key
-                    if (currentCount.containsKey(adjKey)) currentCount[adjKey] = currentCount[adjKey]!!.plus(adjCount[adjKey]!!)
-                    else currentCount[adjKey] = adjCount[adjKey]!!
-                }
+
+        // create label map
+        labels.forEachIndexed { i, char ->
+            labelMap[i] = char
+        }
+        recurse(0)
+        return res
+    }
+
+    // Recursively traverse each node in the tree.
+    // Each node receives an Array of count of all the labels from each of it's subtree.
+    fun recurse(node: Int): IntArray {
+        var labelsCount = IntArray(26)
+
+        // Use seen set to ensure we traverse only from root towards children
+        seen.add(node)
+        for (child in edgesMap.getOrDefault(node, mutableListOf<Int>())) {
+            if(child in seen) continue
+
+            // get labels from it's children
+            val labels = recurse(child)
+
+            // update the count of labels so that the parent can use the information
+            for (i in 0..25) {
+                labelsCount[i] += labels[i]
             }
-            count[i] = currentCount
         }
-        val result = IntArray(n) { 1 }
-        repeat(n) {
-            result[it] = count[it]!![labels[it]]!!
-        }
-        return result
+        // include current node's label as well
+        labelsCount[labelMap[node] - 'a']++
+
+        // update the result for this node's label
+        res[node] = labelsCount[labelMap[node] - 'a']
+        return labelsCount
+
     }
 }
 
@@ -71,7 +94,7 @@ private fun test2() {
     println(Prob2().countSubTrees(5, generateIntArray("[[0,1],[0,2],[1,3],[0,4]]"), "aabab").toList())
     println(Prob2().countSubTrees(6, generateIntArray("[[0,1],[0,2],[1,3],[3,4],[4,5]]"), "cbabaa").toList())
     println(Prob2().countSubTrees(7, generateIntArray("[[0,1],[1,2],[2,3],[3,4],[4,5],[5,6]]"), "aaabaaa").toList())
-    println(Prob2().countSubTrees(4, generateIntArray("[[0,2],[0,3],[1,2]"), "aaabaaa").toList())
+    println(Prob2().countSubTrees(4, generateIntArray("[[0,2],[0,3],[1,2]"), "aeed").toList())
 }
 
 private class Prob3 {
