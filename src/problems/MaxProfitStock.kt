923698045e0a3916381e8a42c4d2fe1a1d980262
Randheer <randheer094@gmail.com>
2020-07-29 17:29:00 +0800

    Todays challenge

--- src/problems/MaxProfitStock.kt
+++ src/problems/MaxProfitStock.kt
@@ -1,5 +1,7 @@
 package problems
 
+import kotlin.math.max
+
 /**
  * Created by randheercode
  * Date: 24/5/20
@@ -24,10 +26,32 @@ class MaxProfitStock {
         }
         return profit
     }
+
+    fun maxProfitCoolDown(prices: IntArray): Int {
+        if (prices.size <= 1) return 0
+        if (prices.size == 2) return if (prices[0] > prices[1]) 0 else prices[1] - prices[0]
+        val dp = Array(prices.size) { IntArray(2) }
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+        dp[1][0] = maxOf(dp[0][1] + prices[1], dp[0][0])
+        dp[1][1] = maxOf(dp[0][0] - prices[1], dp[0][1])
+        for (i in 2 until prices.size) {
+            dp[i][0] = maxOf(dp[i - 1][1] + prices[i], dp[i - 1][0])
+            dp[i][1] = maxOf(dp[i - 2][0] - prices[i], dp[i - 1][1])
+        }
+        return dp[prices.size - 1][0]
+    }
 }
 
 fun main() {
-    println(MaxProfitStock().maxProfit(intArrayOf(1, 2, 3, 4, 5)))
-    println(MaxProfitStock().maxProfit(intArrayOf(7, 6, 4, 3, 1)))
-    println(MaxProfitStock().maxProfit(intArrayOf(1, 3, 2, 5, 7, 8, 2, 9, 3, 6, 2)))
+    fun maxProfit() {
+        println(MaxProfitStock().maxProfit(intArrayOf(1, 2, 3, 4, 5)))
+        println(MaxProfitStock().maxProfit(intArrayOf(7, 6, 4, 3, 1)))
+        println(MaxProfitStock().maxProfit(intArrayOf(1, 3, 2, 5, 7, 8, 2, 9, 3, 6, 2)))
+    }
+
+    fun maxProfitCoolDown() {
+        println(MaxProfitStock().maxProfitCoolDown(intArrayOf(1, 2, 3, 0, 2)))
+    }
+    maxProfitCoolDown()
 }
