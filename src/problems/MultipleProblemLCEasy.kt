9d1971c5bf2bdf6cdd878b9564dfec42aa72028d
Randheer <randheer094@gmail.com>
2020-07-30 23:17:57 +0800

    Todays challenge

--- src/problems/MultipleProblemLCEasy.kt
+++ src/problems/MultipleProblemLCEasy.kt
@@ -1656,30 +1656,6 @@ class MultipleProblemLCEasy {
         return dummy.next
     }
 
-    fun wordBreak(s: String, wordDict: List<String>): List<String> {
-        val map = mutableMapOf<Int, MutableList<String>>()
-
-        fun wordBreakHelper(input: String, end: Int, wordDict: List<String>): List<String> {
-            val res = ArrayList<String>()
-            if (end == 0) return listOf("")
-            if (map.containsKey(end)) {
-                return map[end]!!
-            }
-            for (start in 0 until end) {
-                val sub = input.substring(start, end)
-                if (wordDict.contains(sub)) {
-                    val tmpList = wordBreakHelper(input, start, wordDict)
-                    for (tmpStr in tmpList) {
-                        res.add(if (tmpStr.isEmpty()) sub else "$tmpStr $sub")
-                    }
-                }
-            }
-            map[end] = res
-            return res
-        }
-        return wordBreakHelper(s, s.length, wordDict)
-    }
-
 }
 
 
