fec6fcc6a9a0df06eace9b4761ae520fd81f3a71
Randheer <randheer094@gmail.com>
2020-08-13 18:37:08 +0800

    Todays challenge

--- src/problem/Aug20LCJava.java
+++ src/problem/Aug20LCJava.java
@@ -1,9 +1,6 @@
 package problem;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -58,5 +55,47 @@ public class Aug20LCJava {
     public static void main(String[] args) {
         Aug20LCJava obj = new Aug20LCJava();
         Aug20LCJava.hIndexTest(obj);
+    }
+}
+
+class CombinationIterator {
+    public Deque< String > combinations = new ArrayDeque<>();
+
+    public CombinationIterator(String characters, int combinationLength) {
+        int n = characters.length(), k = combinationLength;
+
+        // init the first combination
+        int[] nums = new int[k + 1];
+        for (int i = 0; i < k; ++i) {
+            nums[i] = i;
+        }
+        nums[k] = n;
+
+        int j = 0;
+        while (j < k) {
+            // add current combination
+            StringBuilder sb = new StringBuilder();
+            for (int i = k - 1; i > -1; i--) {
+                sb.append(characters.charAt(n - 1 - nums[i]));
+            }
+            combinations.push(sb.toString());
+            // Generate next combination.
+            // Find the first j such that nums[j] + 1 != nums[j + 1].
+            // Increase nums[j] by one.
+            j = 0;
+            while ((j < k) && (nums[j + 1] == nums[j] + 1)) {
+                nums[j] = j;
+                j++;
+            }
+            nums[j]++;
+        }
+    }
+
+    public String next() {
+        return combinations.pop();
+    }
+
+    public boolean hasNext() {
+        return (!combinations.isEmpty());
     }
 }
