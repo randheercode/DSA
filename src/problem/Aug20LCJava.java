009965d95a82e04008352b474e60eccdf7a35991
Randheer <randheer094@gmail.com>
2020-08-14 18:36:55 +0800

    Todays challenge

--- src/problem/Aug20LCJava.java
+++ src/problem/Aug20LCJava.java
@@ -48,6 +48,20 @@ public class Aug20LCJava {
         return last;
     }
 
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        for (char c: s.toCharArray())
+            count[c]++;
+
+        int ans = 0;
+        for (int v: count) {
+            ans += v / 2 * 2;
+            if (ans % 2 == 0 && v % 2 == 1)
+                ans++;
+        }
+        return ans;
+    }
+
     public static void hIndexTest(Aug20LCJava obj) {
         System.out.println(obj.hIndex(new int[]{3, 0, 6, 1, 5}));
     }
