ba02b37f4bcd998cc59550e26c5eed88ad440add
Randheer <randheer094@gmail.com>
2020-08-16 15:53:51 +0800

    Todays challenge

--- src/problem/Aug20LCJava.java
+++ src/problem/Aug20LCJava.java
@@ -97,6 +97,24 @@ public class Aug20LCJava {
         return res;
     }
 
+    public int maxProfit3(int[] prices) {
+        int t1Cost = Integer.MAX_VALUE,
+                t2Cost = Integer.MAX_VALUE;
+        int t1Profit = 0,
+                t2Profit = 0;
+
+        for (int price : prices) {
+            // the maximum profit if only one transaction is allowed
+            t1Cost = Math.min(t1Cost, price);
+            t1Profit = Math.max(t1Profit, price - t1Cost);
+            // reinvest the gained profit in the second transaction
+            t2Cost = Math.min(t2Cost, price - t1Profit);
+            t2Profit = Math.max(t2Profit, price - t2Cost);
+        }
+
+        return t2Profit;
+    }
+
     public static void main(String[] args) {
         Aug20LCJava obj = new Aug20LCJava();
     }
