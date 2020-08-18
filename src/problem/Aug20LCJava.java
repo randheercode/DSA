ba6c22ce34fa90df7edac6f15b842eb0ee58bfd0
Randheer <randheer094@gmail.com>
2020-08-18 21:46:30 +0800

    Todays challenge

--- src/problem/Aug20LCJava.java
+++ src/problem/Aug20LCJava.java
@@ -128,6 +128,37 @@ public class Aug20LCJava {
         return result;
     }
 
+    public int[] numsSameConsecDiff(int N, int K) {
+        if (N == 1)
+            return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        List<Integer> results = new ArrayList<Integer>();
+        for (int num = 1; num < 10; ++num)
+            this.DFS(N - 1, num, K, results);
+
+
+        return results.stream().mapToInt(i->i).toArray();
+    }
+
+    protected void DFS(int N, int num, int K, List<Integer> results) {
+        if (N == 0) {
+            results.add(num);
+            return;
+        }
+        List<Integer> nextDigits = new ArrayList<>();
+
+        int tailDigit = num % 10;
+        nextDigits.add(tailDigit + K);
+        if (K != 0)
+            nextDigits.add(tailDigit - K);
+        for (Integer nextDigit : nextDigits) {
+            if (0 <= nextDigit && nextDigit < 10) {
+                int newNum = num * 10 + nextDigit;
+                this.DFS(N - 1, newNum, K, results);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Aug20LCJava obj = new Aug20LCJava();
     }
