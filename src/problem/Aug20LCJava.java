86197ea48b4443da0a2159a1aa3b2b6ca6b8e996
Randheer <randheer094@gmail.com>
2020-08-27 18:58:47 +0800

    Todays challenge

--- src/problem/Aug20LCJava.java
+++ src/problem/Aug20LCJava.java
@@ -97,6 +97,25 @@ public class Aug20LCJava {
         return res;
     }
 
+    public int[] findRightInterval(int[][] intervals) {
+        int[][] endIntervals = Arrays.copyOf(intervals, intervals.length);
+        HashMap<int[], Integer> hash = new HashMap<>();
+        for (int i = 0; i < intervals.length; i++) {
+            hash.put(intervals[i], i);
+        }
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        Arrays.sort(endIntervals, (a, b) -> a[1] - b[1]);
+        int j = 0;
+        int[] res = new int[intervals.length];
+        for (int i = 0; i < endIntervals.length; i++) {
+            while (j < intervals.length && intervals[j][0] < endIntervals[i][1]) {
+                j++;
+            }
+            res[hash.get(endIntervals[i])] = j == intervals.length ? -1 : hash.get(intervals[j]);
+        }
+        return res;
+    }
+
     public int maxProfit3(int[] prices) {
         int t1Cost = Integer.MAX_VALUE,
                 t2Cost = Integer.MAX_VALUE;
