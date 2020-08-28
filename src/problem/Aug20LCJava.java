14768d39706d64ad6df85d273b3318e2f0589752
Randheer <randheer094@gmail.com>
2020-08-28 18:38:07 +0800

    Todays challenge

--- src/problem/Aug20LCJava.java
+++ src/problem/Aug20LCJava.java
@@ -97,6 +97,16 @@ public class Aug20LCJava {
         return res;
     }
 
+    /*public int rand10() {
+        int row, col, idx;
+        do {
+            row = rand7();
+            col = rand7();
+            idx = col + (row - 1) * 7;
+        } while (idx > 40);
+        return 1 + (idx - 1) % 10;
+    }*/
+
     public int[] findRightInterval(int[][] intervals) {
         int[][] endIntervals = Arrays.copyOf(intervals, intervals.length);
         HashMap<int[], Integer> hash = new HashMap<>();
