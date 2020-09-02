d5290ccad0be896cbfab09c04b5ab7b7354ef875
Randheer <randheer094@gmail.com>
2020-09-02 17:36:39 +0800

    Todays challenge

--- src/problem/Aug20LCJava.java
+++ src/problem/Aug20LCJava.java
@@ -358,7 +358,6 @@ public class Aug20LCJava {
         return root.val;
     }
 
-
     public TreeNode deleteNode(TreeNode root, int key) {
         if (root == null) return null;
 
@@ -432,6 +431,25 @@ public class Aug20LCJava {
             }
         }
         return result;
+    }
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+            // Find the successor of current element
+            Integer s = set.ceiling(nums[i]);
+            if (s != null && s <= nums[i] + t) return true;
+
+            // Find the predecessor of current element
+            Integer g = set.floor(nums[i]);
+            if (g != null && nums[i] <= g + t) return true;
+
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
