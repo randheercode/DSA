b3341c0a1b028256153ccb8674340244d77523cd
Randheer <randheer094@gmail.com>
2020-09-09 17:18:07 +0800

    Todays challenge

--- src/problem/Aug20LCJava.java
+++ src/problem/Aug20LCJava.java
@@ -369,8 +369,7 @@ public class Aug20LCJava {
             else if (root.right != null) {
                 root.val = successor(root);
                 root.right = deleteNode(root.right, root.val);
-            }
-            else {
+            } else {
                 root.val = predecessor(root);
                 root.left = deleteNode(root.left, root.val);
             }
@@ -490,8 +489,8 @@ public class Aug20LCJava {
     }
 
     public boolean wordPattern(String pattern, String str) {
-        HashMap <Character, String> map_char = new HashMap();
-        HashMap <String, Character> map_word = new HashMap();
+        HashMap<Character, String> map_char = new HashMap();
+        HashMap<String, Character> map_word = new HashMap();
         String[] words = str.split(" ");
 
         if (words.length != pattern.length())
@@ -516,6 +515,24 @@ public class Aug20LCJava {
         }
 
         return true;
+    }
+
+    public int compareVersion(String version1, String version2) {
+        String[] nums1 = version1.split("\\.");
+        String[] nums2 = version2.split("\\.");
+        int n1 = nums1.length, n2 = nums2.length;
+
+        // compare versions
+        int i1, i2;
+        for (int i = 0; i < Math.max(n1, n2); ++i) {
+            i1 = i < n1 ? Integer.parseInt(nums1[i]) : 0;
+            i2 = i < n2 ? Integer.parseInt(nums2[i]) : 0;
+            if (i1 != i2) {
+                return i1 > i2 ? 1 : -1;
+            }
+        }
+        // the versions are equal
+        return 0;
     }
 
     public static void main(String[] args) {
