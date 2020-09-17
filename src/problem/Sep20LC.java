f5830d62cf02bbd5742feccadd45bfe0b5778be4
Randheer <randheer094@gmail.com>
2020-09-17 15:52:51 +0800

    Todays challenge

--- src/problem/Sep20LC.java
+++ src/problem/Sep20LC.java
@@ -38,4 +38,21 @@ public class Sep20LC {
         }
         return maxXor;
     }
+
+    public boolean isRobotBounded(String instructions) {
+        int[][] directions = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int x = 0, y = 0;
+        int dir = 0;
+        for (char i : instructions.toCharArray()) {
+            if (i == 'L')
+                dir = (dir + 3) % 4;
+            else if (i == 'R')
+                dir = (dir + 1) % 4;
+            else {
+                x += directions[dir][0];
+                y += directions[dir][1];
+            }
+        }
+        return (x == 0 && y == 0) || (dir != 0);
+    }
 }
