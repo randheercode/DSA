2ec257996eb58c006c235909dbf314f6b50353ec
Randheer <randheer094@gmail.com>
2020-09-18 16:53:19 +0800

    Todays challenge

--- src/problem/Sep20LC.java
+++ src/problem/Sep20LC.java
@@ -55,4 +55,17 @@ public class Sep20LC {
         }
         return (x == 0 && y == 0) || (dir != 0);
     }
+
+    public int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+
 }
