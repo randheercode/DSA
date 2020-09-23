7c6f795ed83ff8641e3029230d4d9e398d1eee64
Randheer <randheer094@gmail.com>
2020-09-23 16:54:45 +0800

    Todays challenge

--- src/problem/Sep20LC.java
+++ src/problem/Sep20LC.java
@@ -1,6 +1,9 @@
 package problem;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -107,7 +110,7 @@ public class Sep20LC {
         Integer candidate1 = null;
         Integer candidate2 = null;
 
-        for (int n: nums) {
+        for (int n : nums) {
             if (candidate1 != null && candidate1 == n) {
                 count1++;
             } else if (candidate2 != null && candidate2 == n) {
@@ -125,21 +128,41 @@ public class Sep20LC {
         }
 
         // 2nd pass
-        List result = new ArrayList <> ();
+        List result = new ArrayList<>();
 
         count1 = 0;
         count2 = 0;
 
-        for (int n: nums) {
+        for (int n : nums) {
             if (candidate1 != null && n == candidate1) count1++;
             if (candidate2 != null && n == candidate2) count2++;
         }
 
         int n = nums.length;
-        if (count1 > n/3) result.add(candidate1);
-        if (count2 > n/3) result.add(candidate2);
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
 
         return result;
+    }
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+
+        int total_tank = 0;
+        int curr_tank = 0;
+        int starting_station = 0;
+        for (int i = 0; i < n; ++i) {
+            total_tank += gas[i] - cost[i];
+            curr_tank += gas[i] - cost[i];
+            // If one couldn't get here,
+            if (curr_tank < 0) {
+                // Pick up the next station as the starting one.
+                starting_station = i + 1;
+                // Start with an empty tank.
+                curr_tank = 0;
+            }
+        }
+        return total_tank >= 0 ? starting_station : -1;
     }
 
 }
