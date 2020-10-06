c4126105f880216d5110b697762ab741b4a117f6
Randheer <randheer094@gmail.com>
2020-10-06 23:19:33 +0800

    FB practise

--- src/problem/facebook/TaskScheduler.kt
+++ src/problem/facebook/TaskScheduler.kt
@@ -2,7 +2,7 @@ package problem.facebook
 
 import java.util.*
 
-
+// https://leetcode.com/problems/task-scheduler/solution/
 class TaskScheduler {
     fun leastInterval(tasks: CharArray, n: Int): Int {
         val f = IntArray(26)
@@ -15,6 +15,28 @@ class TaskScheduler {
             idleSpots -= minOf(chunk, f[i])
         }
         return if (idleSpots < 0) tasks.size else idleSpots + tasks.size
+    }
+
+    fun leastIntervalMath(tasks: CharArray, n: Int): Int {
+        // frequencies of the tasks
+        val frequencies = IntArray(26)
+        for (t in tasks) {
+            frequencies[t.toInt() - 'A'.toInt()]++
+        }
+
+        // max frequency
+        var f_max = 0
+        for (f in frequencies) {
+            f_max = maxOf(f_max, f)
+        }
+
+        // count the most frequent tasks
+        var n_max = 0
+        for (f in frequencies) {
+            if (f == f_max) n_max++
+        }
+
+        return maxOf(tasks.size, (f_max - 1) * (n + 1) + n_max)
     }
 }
 
