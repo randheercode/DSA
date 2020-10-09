ff4e715c1aa32f3ca7253e4baedcba6f7d21ae58
Randheer <randheer094@gmail.com>
2020-10-09 16:59:59 +0800

    FB practise

--- src/problem/facebook/IntegerToRoman.kt
+++ src/problem/facebook/IntegerToRoman.kt
@@ -2,17 +2,28 @@ package problem.facebook
 
 // https://leetcode.com/problems/integer-to-roman/
 class IntegerToRoman {
-    fun intToRoman(num: Int): String {
-        val result = StringBuilder()
-        result.insert(4,'0')
-        return result.toString()
+    var values = intArrayOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    var symbols = arrayOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
+
+    fun intToRoman(num: Int): String? {
+        var n = num
+        val sb = StringBuilder()
+        var i = 0
+        while (i < values.size && n >= 0) {
+            while (values[i] <= n) {
+                n -= values[i]
+                sb.append(symbols[i])
+            }
+            i++
+        }
+        return sb.toString()
     }
 }
 
 fun main() {
     println(IntegerToRoman().intToRoman(3))
-//    println(IntegerToRoman().intToRoman(4))
-//    println(IntegerToRoman().intToRoman(9))
-//    println(IntegerToRoman().intToRoman(58))
-//    println(IntegerToRoman().intToRoman(1994))
+    println(IntegerToRoman().intToRoman(4))
+    println(IntegerToRoman().intToRoman(9))
+    println(IntegerToRoman().intToRoman(58))
+    println(IntegerToRoman().intToRoman(1994))
 }
