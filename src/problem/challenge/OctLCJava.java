00ea148a68c8c06e640dba66ea8657d54fafe662
Randheer <randheer094@gmail.com>
2020-10-30 18:15:21 +0800

    Todays challenge

--- src/problem/challenge/OctLCJava.java
+++ src/problem/challenge/OctLCJava.java
@@ -505,16 +505,49 @@ public class OctLCJava {
             }
         }
 
-        for (int i = 0; i < N; ++i)  if (seats[i] == 1) {
-            ans = Math.max(ans, i);
-            break;
+        for (int i = 0; i < N; ++i)
+            if (seats[i] == 1) {
+                ans = Math.max(ans, i);
+                break;
+            }
+
+        for (int i = N - 1; i >= 0; --i)
+            if (seats[i] == 1) {
+                ans = Math.max(ans, N - 1 - i);
+                break;
+            }
+
+        return ans;
+    }
+
+    public int findNumberOfLIS(int[] nums) {
+        int N = nums.length;
+        if (N <= 1) return N;
+        int[] lengths = new int[N];
+        int[] counts = new int[N];
+        Arrays.fill(counts, 1);
+
+        for (int j = 0; j < N; ++j) {
+            for (int i = 0; i < j; ++i)
+                if (nums[i] < nums[j]) {
+                    if (lengths[i] >= lengths[j]) {
+                        lengths[j] = lengths[i] + 1;
+                        counts[j] = counts[i];
+                    } else if (lengths[i] + 1 == lengths[j]) {
+                        counts[j] += counts[i];
+                    }
+                }
         }
 
-        for (int i = N-1; i >= 0; --i)  if (seats[i] == 1) {
-            ans = Math.max(ans, N - 1 - i);
-            break;
+        int longest = 0, ans = 0;
+        for (int length : lengths) {
+            longest = Math.max(longest, length);
         }
-
+        for (int i = 0; i < N; ++i) {
+            if (lengths[i] == longest) {
+                ans += counts[i];
+            }
+        }
         return ans;
     }
 }
