43594c53a5bffb98f229ecd6dd67c8458ec95add
Randheer <randheer094@gmail.com>
2020-11-01 00:20:53 +0800

    Todays challenge

--- src/problem/lc_contest/BW38.kt
+++ src/problem/lc_contest/BW38.kt
@@ -1,8 +1,47 @@
 package problem.lc_contest
 
 class BW38 {
-    fun test1(): Int {
-        return 1;
+
+    fun frequencySort(nums: IntArray): IntArray {
+        val freqMap = mutableMapOf<Int, Int>()
+        for (num in nums) freqMap[num] = freqMap.getOrDefault(num, 0) + 1
+        val sortedNum = nums.sortedWith(Comparator { a, b ->
+            if (freqMap[a]!! != freqMap[b]) {
+                freqMap[a]!! - freqMap[b]!!
+            } else {
+                b - a
+            }
+        })
+        return sortedNum.toIntArray()
     }
+
+    fun maxWidthOfVerticalArea(points: Array<IntArray>): Int {
+        points.sortBy { it[0] }
+        var ans = 0
+        for (i in 1 until points.size) {
+            ans = maxOf(ans, points[i][0] - points[i - 1][0])
+        }
+        return ans
+    }
+
+    fun countSubstrings(s: String, t: String): Int {
+        var ans = 0
+        for (i in s.indices) {
+            for (j in t.indices) {
+                var x = i
+                var y = j
+                var diff = 0
+                while (x < s.length && y < t.length) {
+                    if (s[x] != t[y]) diff++
+                    if (diff == 1) ans++
+                    if (diff == 2) break
+                    x++
+                    y++
+                }
+            }
+        }
+        return ans
+    }
+
 }
 
--- test/problem/ic_contest/BW38Test.kt
+++ test/problem/ic_contest/BW38Test.kt
@@ -1,5 +1,6 @@
 package problem.ic_contest
 
+import generateIntArray
 import org.junit.Assert
 import org.junit.Test
 import problem.lc_contest.BW38
@@ -8,8 +9,21 @@ class BW38Test {
     private val obj = BW38()
 
     @Test
-    fun test1() {
-        Assert.assertEquals(1, obj.test1())
+    fun frequencySort() {
+        Assert.assertEquals("[3, 1, 1, 2, 2, 2]", obj.frequencySort(intArrayOf(1, 1, 2, 2, 2, 3)).toList().toString())
+        Assert.assertEquals("[1, 3, 3, 2, 2]", obj.frequencySort(intArrayOf(2, 3, 1, 3, 2)).toList().toString())
+        Assert.assertEquals("[5, -1, 4, 4, -6, -6, 1, 1, 1]", obj.frequencySort(intArrayOf(-1, 1, -6, 4, 5, -6, 1, 4, 1)).toList().toString())
+    }
+
+    @Test
+    fun maxWidthOfVerticalArea() {
+        Assert.assertEquals(1, obj.maxWidthOfVerticalArea(generateIntArray("[[8,7],[9,9],[7,4],[9,7]]")))
+        Assert.assertEquals(3, obj.maxWidthOfVerticalArea(generateIntArray("[[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]")))
+    }
+
+    @Test
+    fun countSubstrings() {
+        Assert.assertEquals(6, obj.countSubstrings("aba", "baba"))
     }
 }
 
