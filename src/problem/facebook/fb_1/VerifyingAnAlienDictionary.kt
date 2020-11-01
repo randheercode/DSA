0a8f3c6e248de2f6d6e12a8984c29717de7ecd1c
Randheer <randheer094@gmail.com>
2020-11-01 21:08:55 +0800

    FB practise

--- src/problem/facebook/fb_1/VerifyingAnAlienDictionary.kt
+++ src/problem/facebook/fb_1/VerifyingAnAlienDictionary.kt
@@ -6,4 +6,37 @@ package problem.facebook.fb_1
  * Time: 10:42 am
  */
 class VerifyingAnAlienDictionary {
+
+    private val orderMap = mutableMapOf<Char, Int>()
+
+    private fun prepareOrder(order: String) {
+        for (i in order.indices) {
+            orderMap[order[i]] = i
+        }
+    }
+
+    private fun indexOf(char: Char): Int {
+        return orderMap[char]!!
+    }
+
+    fun isAlienSorted(words: Array<String>, order: String): Boolean {
+        prepareOrder(order)
+        for (idx in 1 until words.size) {
+            val prev = words[idx - 1]
+            val curr = words[idx]
+            var diffStart = -1
+            for (i in 0 until minOf(prev.length, curr.length)) {
+                if (indexOf(curr[i]) != indexOf(prev[i])) {
+                    if (indexOf(curr[i]) > indexOf(prev[i])) {
+                        diffStart = i
+                        break
+                    } else {
+                        return false
+                    }
+                }
+            }
+            if (diffStart == -1 && prev.length > curr.length) return false
+        }
+        return true
+    }
 }
--- test/problem/facebook/fb_1/VerifyingAnAlienDictionaryTest.kt
+++ test/problem/facebook/fb_1/VerifyingAnAlienDictionaryTest.kt
@@ -1,9 +1,21 @@
 package problem.facebook.fb_1
 
+import org.junit.Assert
+import org.junit.Test
+
 /**
  * Created by randheercode
  * Date: 30/10/20
  * Time: 10:42 am
  */
 class VerifyingAnAlienDictionaryTest {
+    private val obj = VerifyingAnAlienDictionary()
+
+    @Test
+    fun isAlienSorted() {
+        Assert.assertTrue(obj.isAlienSorted(arrayOf("hello", "leetcode"), "hlabcdefgijkmnopqrstuvwxyz"))
+        Assert.assertFalse(obj.isAlienSorted(arrayOf("word", "world", "row"), "worldabcefghijkmnpqstuvxyz"))
+        Assert.assertFalse(obj.isAlienSorted(arrayOf("apple", "app"), "abcdefghijklmnopqrstuvwxyz"))
+    }
+
 }
