e72e810ae21bb58e3c083fd633cde74d83cf5412
Randheer <randheer094@gmail.com>
2020-11-01 17:22:30 +0800

    Todays challenge

--- src/problem/challenge/NovLC.kt
+++ src/problem/challenge/NovLC.kt
@@ -1,4 +1,37 @@
 package problem.challenge
 
+import java.lang.StrictMath.pow
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
 class NovLC {
+
+    fun canAttendMeetings(intervals: Array<IntArray>): Boolean {
+        intervals.sortBy { it[0] }
+        for (i in 1 until intervals.size) {
+            if (intervals[i - 1][1] > intervals[i][0]) return false
+        }
+        return true
+    }
+
+    fun getDecimalValue(head: ListNode?): Int {
+        var count = 0
+        var current = head
+        while (current?.next != null) {
+            current = current.next
+            count++
+        }
+        var result = 0
+        current = head
+        while (current != null) {
+            if (current.`val` == 1) {
+                result += pow(2.0, count.toDouble()).toInt()
+            }
+            current = current.next
+            count--
+        }
+        return result
+    }
 }
--- test/problem/challenge/NovLCTest.kt
+++ test/problem/challenge/NovLCTest.kt
@@ -1,7 +1,24 @@
 package problem.challenge
 
+import generateIntArray
+import org.junit.Assert
+import org.junit.Test
+
 class NovLCTest {
+
     private val obj = NovLC()
+
+    @Test
+    fun canAttendMeetings() {
+        Assert.assertFalse(obj.canAttendMeetings(generateIntArray("[[0,30],[5,10],[15,20]]")))
+        Assert.assertTrue(obj.canAttendMeetings(generateIntArray("[[7,10],[2,4]]")))
+    }
+
+    @Test
+    fun getDecimalValue() {
+        Assert.assertEquals(0, obj.getDecimalValue(ListNode(0)))
+        Assert.assertEquals(1, obj.getDecimalValue(ListNode(1)))
+    }
 
 }
 
