cf67a9f277b81be42b52d90ea76fa352cd2467c2
Randheer <randheer094@gmail.com>
2020-11-15 11:25:31 +0800

    Todays challenge

--- src/problem/lc_contest/W215.kt
+++ src/problem/lc_contest/W215.kt
@@ -1,13 +1,13 @@
 package problem.lc_contest
 
+import kotlin.math.abs
+
 
 class W215 {
 
     class OrderedStream(val n: Int) {
-
         val data = Array<String>(n + 1) { "" }
         var ptr = 1
-
         fun insert(id: Int, value: String): List<String> {
             data[id] = value
             val result = mutableListOf<String>()
@@ -19,7 +19,48 @@ class W215 {
             ptr = idx
             return result
         }
+    }
 
+    // Wrong Answer
+    fun closeStrings(word1: String, word2: String): Boolean {
+        if (word1.length != word2.length) return false
+
+        if (word1 == word2) return true
+
+        val diff = IntArray(26)
+
+        val count1 = IntArray(26)
+        for (c in word1) count1[c - 'a'] += 1
+
+        val count2 = IntArray(26)
+        for (c in word2) {
+            count2[c - 'a'] += 1
+            diff[c - 'a'] = count1[c - 'a'] - count2[c - 'a']
+        }
+
+        if (diff.all { it == 0 }) return true
+
+        val count = mutableMapOf<Int, Int>()
+
+        for (d in diff) count[abs(d)] = count.getOrDefault(abs(d), 0) + 1
+
+        return count.all { it.value == 2 }
+    }
+
+    private fun countOps(nums: IntArray, sIdx: Int, eIdx: Int, x: Int, count: Int): Int {
+
+        if (x == 0) return count
+        if (x < 0 || sIdx == nums.size || eIdx == -1 || eIdx < sIdx) return Int.MAX_VALUE
+
+        return minOf(
+                countOps(nums, sIdx + 1, eIdx, x - nums[sIdx], count + 1),
+                countOps(nums, sIdx, eIdx - 1, x - nums[eIdx], count + 1))
+
+    }
+
+    fun minOperations(nums: IntArray, x: Int): Int {
+        val result = countOps(nums, 0, nums.lastIndex, x, 0)
+        return if (result == Int.MAX_VALUE) -1 else result
     }
 
 }
--- test/problem/ic_contest/W215Test.kt
+++ test/problem/ic_contest/W215Test.kt
@@ -1,5 +1,6 @@
 package problem.ic_contest
 
+import org.junit.Assert
 import org.junit.Test
 import problem.lc_contest.W215
 
@@ -8,7 +9,21 @@ class W215Test {
     private val obj = W215()
 
     @Test
-    fun test() {
+    fun closeStrings() {
+        Assert.assertTrue(obj.closeStrings("abc", "bca"))
+        Assert.assertFalse(obj.closeStrings("a", "aa"))
+        Assert.assertTrue(obj.closeStrings("cabbba", "abbccc"))
+        Assert.assertFalse(obj.closeStrings("cabbba", "aabbss"))
+        Assert.assertFalse(obj.closeStrings("abbzzca", "babzzcz"))
+    }
+
+    @Test
+    fun minOps() {
+        Assert.assertEquals(2, obj.minOperations(intArrayOf(1, 1, 4, 2, 3), 5))
+        Assert.assertEquals(-1, obj.minOperations(intArrayOf(5, 6, 7, 8, 9), 4))
+        Assert.assertEquals(5, obj.minOperations(intArrayOf(3, 2, 20, 1, 1, 3), 10))
+        Assert.assertEquals(-1, obj.minOperations(intArrayOf(1, 1), 3))
+        Assert.assertEquals(16, obj.minOperations(intArrayOf(8828, 9581, 49, 9818, 9974, 9869, 9991, 10000, 10000, 10000, 9999, 9993, 9904, 8819, 1231, 6309), 134365))
     }
 
 }
